5405075210efe47ed6ff2a998539e2108a42ded7
ahmed <ahmedabdelnaser3010@gmail.com>
2025-05-19 12:13:06 +0200

    8. Square

---
+++ 8-square.js
@@ -0,0 +1,10 @@
+const firstArgu = parseInt(process.argv[2])
+
+if (isNaN(firstArgu)) {
+    console.log("Missing size")
+} else {
+    for (let i = 0; i < firstArgu; i++) {
+        console.log("x".repeat(firstArgu))
+    }
+}
+
