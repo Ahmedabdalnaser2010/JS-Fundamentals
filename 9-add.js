b0c2ff5cafd1d59cbaa15a7c21ee739df706eadc
ahmed <ahmedabdelnaser3010@gmail.com>
2025-05-19 12:50:23 +0200

    9. Add

---
+++ 9-add.js
@@ -0,0 +1,8 @@
+const firstArgu = parseInt(process.argv[2])
+const secondArgu = parseInt(process.argv[3])
+
+function adding(a, b) {
+    return (a + b)
+}
+
+console.log(adding(firstArgu, secondArgu))
